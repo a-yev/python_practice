d661a99002eccec0bd5666023e35d7ba6fca4d39
a-yev <itwebtechnician@gmail.com>
2020-03-16 20:27:24 +0100

    PEP8. Optimisation for 1,2,3 exercises.

--- Exercises/2. Odd or Even.py
+++ Exercises/2. Odd or Even.py
@@ -1,18 +1,15 @@
-'''
-Ask the user for a number. Depending on whether the number is even or odd,
-print out an appropriate message to the user.
-Hint: how does an even / odd number react differently when divided by 2?
+# Ask the user for a number. Depending on whether the number is even or odd,
+# print out an appropriate message to the user.
+# Hint: how does an even / odd number react differently when divided by 2?
+# Extras:
+# If the number is a multiple of 4, print out a different message.
+# Ask the user for two numbers: one number to check (call it num)
+# and one number to divide by (check).
+# If check divides evenly into num, tell that to the user.
+# If not, print a different appropriate message.
 
-Extras:
-
-If the number is a multiple of 4, print out a different message.
-Ask the user for two numbers: one number to check (call it num)
-and one number to divide by (check).
-If check divides evenly into num, tell that to the user.
-If not, print a different appropriate message.
-'''
-#start
-#importing modules
+# start
+# importing modules
 import sys
 
 # numbers input
@@ -54,4 +51,3 @@ print('The result of dividing {} by {} is {} '.
 
 # exit from python
 sys.exit()
-
