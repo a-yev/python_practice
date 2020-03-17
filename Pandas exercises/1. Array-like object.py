06ed65c489f40a57137838181d2adab72ee99d7a
a-yev <itwebtechnician@gmail.com>
2020-03-17 21:17:00 +0100

    Pandas. Exercise 1. Array-like object

---
+++ Pandas exercises/1. Array-like object.py
@@ -0,0 +1,17 @@
+# Write a Pandas program to create and display a one-dimensional array-like object containing an array of data.
+
+#start
+
+#import and check pandas
+import pandas as pd
+print(pd.__version__)
+
+#creating array
+a = pd.Series([3,5,6,2,1,3,8,6,7,9,0,23,64,2,1,8,0])
+print(a)
+
+
+
+
+
+
