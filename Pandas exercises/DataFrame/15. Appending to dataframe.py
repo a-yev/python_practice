0f81dbf0e1b8b0b56624a28b09ffbb1f4eb893ec
a-yev <itwebtechnician@gmail.com>
2020-04-15 22:07:08 +0200

    15. Appending new row to dataframe

---
+++ Pandas exercises/DataFrame/15. Appending to dataframe.py
@@ -0,0 +1,27 @@
+# Write a Pandas program to append a new row 'b' to data frame with given values for each column.
+# Now delete the new row and return the original DataFrame.
+
+# start
+# import modules
+
+import pandas as pd
+import numpy as np
+
+results = {'surname': ['Dvorak', 'Nowacki', 'Harket', 'Lundt',
+'Jensen', 'Kardif', 'Bergger', 'Aronson', 'Verin', 'Ulemaa'],
+        'score': [1.5, 9, 16.5, np.nan, 20, 12, 4.5, np.nan, 8, 18],
+        'attempts': [2, 1, 1, 4, 1, 2, 2, 3, 1, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no',
+        'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(results , index=labels)
+print("Orginal rows:")
+print(df)
+print("\nAppend a new row:")
+df.loc['b'] = [1, 'Suresh', 'yes', 15.5]
+print("Print all records after insert a new record:")
+print(df)
+print("\nDelete the new row and display the original  rows:")
+df = df.drop('b')
+print(df)
