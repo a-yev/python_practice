c2694e9e8c504771020c3589de89c51ce315d192
a-yev <itwebtechnician@gmail.com>
2020-04-30 21:20:22 +0200

    21. Iterating over rows in dataframe

---
+++ Pandas exercises/DataFrame/21. Iterate over rows in a DataFrame.py
@@ -0,0 +1,19 @@
+# Write a Pandas program to iterate over rows in a DataFrame.
+
+# start
+# import modules
+
+import pandas as pd
+import numpy as np
+
+results = {'surname': ['Dvorak', 'Nowacki', 'Harket', 'Lundt',
+                       'Jensen', 'Kardif', 'Bergger', 'Aronson', 'Verin', 'Ulemaa'],
+           'score': [1.5, 9, 16.5, np.nan, 20, 12, 4.5, np.nan, 8, 18],
+           'attempts': [2, 1, 1, 4, 1, 2, 2, 3, 1, 1],
+           'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no',
+                       'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(results)
+for index, row in df.iterrows():
+    print(row['surname'], row['score'])
