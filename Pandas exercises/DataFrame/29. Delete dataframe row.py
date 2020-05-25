76ba07744605e3549f4ca510d733bd919d12ea0b
a-yev <itwebtechnician@gmail.com>
2020-05-25 22:34:32 +0200

    29. Deliting dataframe row

---
+++ Pandas exercises/DataFrame/29. Delete dataframe row.py
@@ -0,0 +1,13 @@
+# Write a Pandas program to delete DataFrame row(s) based on given column value.
+
+# start
+# import modules
+
+import pandas as pd
+import numpy as np
+
+d = {'col1': [1, 4, 3, 4, 5], 'col2': [4, 5, 6, 7, 8], 'col3': [7, 8, 9, 0, 1]}
+df = pd.DataFrame(data=d)
+print(("Original DataFrame: ") + str(df))
+df = df[df.col2 != 5]
+print(("New DataFrame") + str(df))
